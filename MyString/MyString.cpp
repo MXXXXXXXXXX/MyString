9e455aeb42438a563d9af60a8d70fb2c275fac5b
zzx <1248232182@qq.com>
2017-06-09 09:55:52 +0800

    添加项目文件。

---
+++ MyString/MyString.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <utility>
+#include <string.h>
+
+class MyString {
+private:
+	char* _data;
+
+public:
+	MyString() : _data(new char[1]) {
+		*_data = '\0';
+	}
+
+	explicit MyString(const char* str) : _data(new char[strlen(str) + 1]) {
+		strcpy(_data, str);
+	}
+
+	MyString(const MyString& rhs) : _data(new char[rhs.size() + 1]) {
+		strcpy(_data, rhs.c_str());
+	}
+	// 或者可以使用C++11的委托来构造函数
+	// MyString(const MyString& rhs) : MyString(rhs._data) { }
+
+	MyString(MyString&& rhs) : _data(rhs._data) {
+		rhs._data = nullptr;
+	}
+
+	~MyString() {
+		delete[] _data;
+	}
+
+	MyString& operator= (MyString rhs) { // pass by value
+		swap(rhs);
+		return *this;
+	}
+	
+	MyString& operator= (MyString&& rhs) {
+		swap(rhs);
+		return *this;
+	}
+
+	MyString& operator* (MyString* rhs)
+	{
+		return MyString(rhs->_data);
+	}
+
+	char& operator[] (size_t n) {
+		return _data[n];
+	}
+
+	size_t size() const {
+		return strlen(_data);
+	}
+
+	const char* c_str() const {
+		return _data;
+	}
+
+	void swap(MyString& rhs) {
+		std::swap(_data, rhs._data);
+	}
+	friend std::istream& operator>>(std::istream& IS, MyString &S);
+};
+
+inline bool operator== (const MyString& lhs, const MyString& rhs) {
+	return !strcmp(lhs.c_str(), rhs.c_str());
+}
+
+inline bool operator== (const MyString& lhs, const char* rhs) {
+	return !strcmp(lhs.c_str(), rhs);
+}
+
+inline bool operator== (const char* lhs, const MyString& rhs) {
+	return !strcmp(lhs, rhs.c_str());
+}
+
+inline std::ostream& operator<<(std::ostream& OS,const MyString &S){
+	OS << S.c_str();
+	return OS;
+}
+
+inline std::istream& operator>>(std::istream& IS, MyString &S){
+	char *input = new char[100];
+	IS.getline(input, 100);
+	size_t i = strlen(input);
+	/*while (input[i] != '\0') i++;*/
+	delete[] S._data;
+	S._data = new char[i + 1];
+	strcpy(S._data, input);
+	delete[] input;
+	return IS;
+}
---
+++ MyString/test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include "MyString.cpp"
+
+void func1(MyString x) {//测试构造函数是否正常（无需函数体）
+
+}
+
+void func2(const MyString& x) {
+
+}
+
+MyString func3() {
+	MyString ret("world");
+	return ret;
+}
+
+int main() {
+	MyString s0;
+	MyString s1("Hello,World");
+	MyString s2(s0);
+	MyString s3 = s1;
+	MyString s8;
+	s8 = s2 = s1;//测试多重赋值（assignment运算符）
+
+	func1(s1);
+	func2(s1);
+	func1(MyString("literal"));//只能进行显示转换（构造函数是explicit）
+	func2(MyString("litera2"));
+	MyString s4 = func3();
+
+	std::vector<MyString> svec;
+	svec.push_back(s0);
+	svec.push_back(s1);
+	svec.push_back(func3());
+	svec.push_back(MyString("test finished."));
+
+	MyString *s5 = &s1;//测试*运算符
+	MyString s6 = *s5;
+
+	std::cout << s6 << std::endl;//测试<<运算符
+
+	char tes = s6[4];//测试[]运算符
+
+	std::cin >> s0;//测试>>运算符
+	std::cout << s0;
+
+	return 0;
+}
